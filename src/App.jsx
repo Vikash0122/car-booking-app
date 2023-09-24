28923334230c286c711958b17946a2eb9c0f6e91
Ritik Jha <ritikj.ug21.ee@nitp.ac.in>
2023-09-24 16:34:01 +0530

    first commit~

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import FeedbackCard from './Components/FeedbackCard'
+import NavBar from './Components/NavBar'
+
+function App() {
+ 
+
+  return (
+    <>
+      <NavBar/>
+      <FeedbackCard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/FeedbackCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FeedbackCard() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default FeedbackCard
